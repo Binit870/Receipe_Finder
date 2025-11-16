1ab2dc1965853b8ab87914b8093c304a709dbe7d
Binit870 <binitpatra870@gmail.com>
2025-11-16 21:47:29 +0530

    Fix case-sensitive filename

--- backend/server.js
+++ backend/server.js
@@ -45,7 +45,8 @@ app.use(express.json());
 
 // ✅ CORS
 const allowedOrigins = [
-  process.env.FRONTEND_URL, // <-- your deployed Vercel frontend
+  process.env.VITE_BACKEND_URL
+, // <-- your deployed Vercel frontend
   "http://localhost:5173",  // <-- for local dev
   "http://localhost:5174",  // <-- optional (if you used another port)
 ];
